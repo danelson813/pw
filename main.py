18f738626852319f7032498e530d2d017bfb40d5
Dan Nelson <dan.nelson1@gmail.com>
2025-09-14 00:16:16 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.firefox.launch(headless=True)
+    page = browser.new_page()
+    page.goto("https://books.toscrape.com")
+    page.wait_for_selector("article")
+    tweets = page.query_selector_all("article a")
+    for t in tweets:
+        print(t.inner_text().strip())
+    browser.close()
